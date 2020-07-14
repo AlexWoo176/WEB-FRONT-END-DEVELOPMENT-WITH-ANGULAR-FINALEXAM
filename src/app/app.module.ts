deb5ca4924b2bb06a128d745bde6f35df48fd81c
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-07-14 08:50:57 +0700

    add components + services + interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListBookComponent } from './components/list-book/list-book.component';
+import { NewBookComponent } from './components/new-book/new-book.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
+import { DetailBookComponent } from './components/detail-book/detail-book.component';
+import { DeleteBookComponent } from './components/delete-book/delete-book.component';
+import { SearchBookComponent } from './components/search-book/search-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListBookComponent,
+    NewBookComponent,
+    EditBookComponent,
+    DetailBookComponent,
+    DeleteBookComponent,
+    SearchBookComponent
   ],
   imports: [
     BrowserModule,
