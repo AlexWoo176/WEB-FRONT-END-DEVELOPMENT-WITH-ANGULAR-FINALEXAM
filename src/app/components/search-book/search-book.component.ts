2b39684a39efb30bfd5b8f7004450fbf163b8e85
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-07-14 09:29:31 +0700

    Add SearchBook Completed

--- src/app/components/search-book/search-book.component.ts
+++ src/app/components/search-book/search-book.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+import {IBook} from '../../models/ibook';
+import {BookManagerService} from '../../services/book-manager.service';
+import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-book',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-book.component.css']
 })
 export class SearchBookComponent implements OnInit {
+  books$: Observable<IBook[]>;
+  private searchedSubject = new Subject<string>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private bookManagerService: BookManagerService) {
   }
 
+  ngOnInit(): void {
+    this.books$ = this.searchedSubject.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((searchedString: string) => this.bookManagerService.searchBook(searchedString))
+    );
+  }
+
+  search(searchedString: string): void {
+    console.log(`searchedString = ${searchedString}`);
+    this.searchedSubject.next(searchedString);
+  }
 }
