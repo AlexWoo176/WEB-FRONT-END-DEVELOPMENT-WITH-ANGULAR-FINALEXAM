ed834efb11909137888234274ac70cf3f449aeba
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-07-14 09:11:52 +0700

    add component NewBook Completed

--- src/app/components/new-book/new-book.component.ts
+++ src/app/components/new-book/new-book.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {IBook} from '../../models/ibook';
+import {BookManagerService} from '../../services/book-manager.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-new-book',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-book.component.css']
 })
 export class NewBookComponent implements OnInit {
+  formBook: FormGroup;
+  book: IBook;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+              private bookManagerService: BookManagerService,
+              private router: Router) {
+    this.book = {
+      id: +'',
+      title: '',
+      author: '',
+      description: ''
+    };
   }
 
+  ngOnInit(): void {
+    this.formBook = this.formBuilder.group({
+      id: ['', [Validators.required]],
+      title: ['', [Validators.required]],
+      author: ['', [Validators.required]],
+      description: ['']
+    });
+  }
+
+  add() {
+    if (this.formBook.valid) {
+      this.bookManagerService.createBook(this.book).subscribe(
+        next => this.router.navigateByUrl('/books'),
+        error => console.log(error)
+      );
+    } else {
+      alert('Data is invalid');
+    }
+  }
 }
